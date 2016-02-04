639fbc1956de6547d383f770ba1468ee5ad84197
Brice Oliver <Brice.Oliver@OntarioSystems.com>
2016-02-04 17:30:57 -0500

    Updated evaluation of default attributes to more closely match the apt cookbook style

--- attributes/default.rb
+++ attributes/default.rb
@@ -28,62 +28,61 @@ default['apt-docker']['supported-codenames'] = {
   wily: true,    # Ubuntu 15.10
 }
 
-if node['platform_family'] == 'debian'
-  uri = 'https://apt.dockerproject.org/repo'
-  distribution = "#{node['platform']}-#{node['lsb']['codename']}"
-  keyserver = 'hkp://p80.pool.sks-keyservers.net:80'
-  key = '58118E89F3A912897C070ADBF76221572C52609D'
+uri = 'https://apt.dockerproject.org/repo'
+codename = node.attribute?('lsb') ? node['lsb']['codename'] : 'notlinux'
+distribution = "#{node['platform']}-#{codename}"
+keyserver = 'hkp://p80.pool.sks-keyservers.net:80'
+key = '58118E89F3A912897C070ADBF76221572C52609D'
 
-  default['apt-docker']['repos'].tap do |repo|
-    repo['docker-main'].tap do |value|
-      # Does this cookbook manage the install of the Docker Main Repo?
-      value['managed'] = true
-      # URI of Docker Main Repo
-      value['uri'] = uri
-      # Package distribution of Docker Main Repo
-      value['distribution'] = distribution
-      # Docker Main Repo Package Groupings
-      value['components'] = ['main']
-      # Whether or not to add the Docker Main Source Repo?
-      value['deb-src'] = false
-      # GPG keyserver for the Docker Main Repo
-      value['keyserver'] = keyserver
-      # GPG fingerprint for the Docker Main Repo
-      value['key'] = key
-    end
+default['apt-docker']['repos'].tap do |repo|
+  repo['docker-main'].tap do |value|
+    # Does this cookbook manage the install of the Docker Main Repo?
+    value['managed'] = true
+    # URI of Docker Main Repo
+    value['uri'] = uri
+    # Package distribution of Docker Main Repo
+    value['distribution'] = distribution
+    # Docker Main Repo Package Groupings
+    value['components'] = ['main']
+    # Whether or not to add the Docker Main Source Repo?
+    value['deb-src'] = false
+    # GPG keyserver for the Docker Main Repo
+    value['keyserver'] = keyserver
+    # GPG fingerprint for the Docker Main Repo
+    value['key'] = key
+  end
 
-    repo['docker-testing'].tap do |value|
-      # Does this cookbook manage the install of the Docker Testing Repo?
-      value['managed'] = false
-      # URI of Docker Testing Repo
-      value['uri'] = uri
-      # Package distribution of Docker Testing Repo
-      value['distribution'] = distribution
-      # Docker Testing Repo Package Groupings
-      value['components'] = ['testing']
-      # Whether or not to add the Docker Main Testing Repo?
-      value['deb-src'] = false
-      # GPG keyserver for the Docker Testing Repo
-      value['keyserver'] = keyserver
-      # GPG fingerprint for the Docker Testing Repo
-      value['key'] = key
-    end
+  repo['docker-testing'].tap do |value|
+    # Does this cookbook manage the install of the Docker Testing Repo?
+    value['managed'] = false
+    # URI of Docker Testing Repo
+    value['uri'] = uri
+    # Package distribution of Docker Testing Repo
+    value['distribution'] = distribution
+    # Docker Testing Repo Package Groupings
+    value['components'] = ['testing']
+    # Whether or not to add the Docker Main Testing Repo?
+    value['deb-src'] = false
+    # GPG keyserver for the Docker Testing Repo
+    value['keyserver'] = keyserver
+    # GPG fingerprint for the Docker Testing Repo
+    value['key'] = key
+  end
 
-    repo['docker-experimental'].tap do |value|
-      # Does this cookbook manage the install of the Docker Experimental Repo?
-      value['managed'] = false
-      # URI of Docker Experimental Repo
-      value['uri'] = uri
-      # Package distribution of Docker Experimental Repo
-      value['distribution'] = distribution
-      # Docker Experimental Repo Package Groupings
-      value['components'] = ['experimental']
-      # Whether or not to add the Docker Experimental Source Repo?
-      value['deb-src'] = false
-      # GPG keyserver for the Docker Experimental Repo
-      value['keyserver'] = keyserver
-      # GPG fingerprint for the Docker Experimental Repo
-      value['key'] = key
-    end
+  repo['docker-experimental'].tap do |value|
+    # Does this cookbook manage the install of the Docker Experimental Repo?
+    value['managed'] = false
+    # URI of Docker Experimental Repo
+    value['uri'] = uri
+    # Package distribution of Docker Experimental Repo
+    value['distribution'] = distribution
+    # Docker Experimental Repo Package Groupings
+    value['components'] = ['experimental']
+    # Whether or not to add the Docker Experimental Source Repo?
+    value['deb-src'] = false
+    # GPG keyserver for the Docker Experimental Repo
+    value['keyserver'] = keyserver
+    # GPG fingerprint for the Docker Experimental Repo
+    value['key'] = key
   end
 end
